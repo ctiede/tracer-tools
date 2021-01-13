74ef1639e2dccde21f96c153f58ff0226484f0cd
Chris <cwt271@nyu.edu>
2021-01-13 13:44:31 -0600

    Determine IDs of tracers that were accreted

---
+++ accreted_tracers.py
@@ -0,0 +1,33 @@
+from argparse import ArgumentParser
+import sys
+import h5py 
+import numpy as np
+
+sys.path.insert(1, '~/MacFadyen/Clemson_Cal/tracer_tools')
+from tracers import TracerData_t
+
+
+def tracers_in_outer_disk(fname):
+    data = TracerData_t(fname)
+    ids  = data.ids()
+    r    = data.radii()
+    return ids[r > 2.5]
+
+def find_accreted_tracers(f_start, f_finish, id_list):
+    id_list  = tracers_in_outer_disk(f_start)
+    data     = TracerData_t(f_finish)
+    ids      = data.ids()
+    r        = data.radii()
+    accreted = ids[r < 0.75]
+    return np.intersect1d(id_list, accreted)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('start' , help='file to start search for accreted tracers')
+    parser.add_argument('finish', help='file to end search for accreted tracers')
+    args = parser.parse_args()
+
+    ids = find_accreted_tracers(args.start, args.finish, ids)
+
+    print("Found {} accreted tracers".format(len(ids)))
+    np.savetxt('accreted_traceres.txt', ids.astype(int))
