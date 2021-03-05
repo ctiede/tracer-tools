c405b9ca071b78f5ee1b840731fdaddc7527a017
Chris <cwt271@nyu.edu>
2021-03-05 13:17:50 -0600

    Refactor to handle >1M tracers
    - Build tracer timeseries without looping over all tracers
    - Only build timeseries for all tracers in a run, so no need to keep
      track of different tracer timeseries files now
      - can slice out individual series in analysis scripts
    - tracer_tseries.h5 now outputs tracer timeseries as an array with shape
      [num_tracers, num_tracer_data_values, num_snapshots]

--- build_tracer_tseries.py
+++ build_tracer_tseries.py
@@ -5,79 +5,34 @@ import numpy as np
 from argparse import ArgumentParser
 from tracers import TracerData_t
 
-class TracerTimeseries:
-    def __init__(self, ID):
-        self.id = ID
-        self.x  = []
-        self.y  = []
-        self.vx = []
-        self.vy = []
-        self.density  = []
-        self.pressure = []
 
-    def append(self, new):
-        self.x.append(       new[1])
-        self.y.append(       new[2])
-        self.vx.append(      new[3])
-        self.vy.append(      new[4])
-        self.density.append( new[5])
-        self.pressure.append(new[6])
+def get_tracer_data(fname, n):
+    print(fname)
+    tracer_heap  = TracerData_t(fname).stack()
+    tracer_array = np.zeros((n, tracer_heap.shape[1]))
+    tracer_ids   = tracer_heap[:,0].astype(int)
+    tracer_array[:,0] = np.arange(n)
+    tracer_array[:,5] = -1 #set density of lost tracers to -1
+    tracer_array[tracer_ids] = tracer_heap
+    return tracer_array
 
-def select_ids(fname, id_file=None):
-    if id_file is None:
-        return TracerData_t(fname).ids()
-    else:
-        return np.loadtxt(id_file)
-
-def append_timeseries(fname, ids, tseries):
-    tracer_data = TracerData_t(fname).stack()
-    for t in tracer_data:
-        ID = int(t[0])
-        if ID not in ids:
-            continue
-        ix, = np.where(ids == ID)[0]
-        tseries[ix].append(t)
-
-def write_tracer_timeseries(group, tseries):
-    group.create_dataset('x', data=tseries.x)
-    group.create_dataset('y', data=tseries.y)
-    group.create_dataset('vx', data=tseries.vx)
-    group.create_dataset('vy', data=tseries.vy)
-    group.create_dataset('density', data=tseries.density)
-    group.create_dataset('pressure', data=tseries.pressure)
 
 if __name__ == '__main__':
 
-    # files   = sys.argv[1:]
     parser = ArgumentParser()
     parser.add_argument('files', nargs='+')
-    parser.add_argument('--id-file', default=None)
     parser.add_argument('--out-file', default='tracer_tseries.h5')
     args = parser.parse_args()  
 
-    # ===============================================================
-    ids     = select_ids(args.files[0], id_file=args.id_file)
-    tseries = np.array([TracerTimeseries(i) for i in ids])
-    ts      = []
+    times = []
+    n_tracers_max = int(np.max(TracerData_t(args.files[0]).ids()) + 1)
+    tracer_series = np.dstack([get_tracer_data(f, n_tracers_max) for f in args.files])
 
-    # ===============================================================
-    for f in args.files:
-        start = time.time()
-        ts.append(h5py.File(f, 'r')['time'][...])
-        append_timeseries(f, ids, tseries)
-        end = time.time()
-        print(f, "[{:.2f} s]".format(end - start))
-
-    # ===============================================================
     fname = args.out_file
-    h5f = h5py.File(fname, 'w')
-    h5f.create_dataset('time', data=ts)
     print("   Saving {}...".format(fname))
 
-    # ===============================================================
-    for t in tseries:
-        g = h5f.create_group("{:d}".format(int(t.id)))
-        write_tracer_timeseries(g, t)
-
-    # ===============================================================
+    h5f = h5py.File(fname, 'w')
+    h5f.create_dataset('time', data=times)
+    h5f.create_dataset('tracer_tseries', data=tracer_series)
     h5f.close()
+    
