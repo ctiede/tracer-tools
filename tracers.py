237d1edd963c7d8f30f0348b6ca3fbba9165260a
Chris <cwt271@nyu.edu>
2021-03-17 11:24:12 -0600

    Add weights to `unpack()`

--- tracers.py
+++ tracers.py
@@ -12,12 +12,14 @@ class TracerData_t:
 
     def unpack(self):
         h5f = h5py.File(self.file, 'r')
+        # unpack = np.vectorize(lambda t: unpack_tracer_data_no_weights(t))
         try:
             unpack = np.vectorize(lambda t: unpack_tracer_data(t))
+            return unpack(h5f['tracers'][...])
         except:
             print("Tracer file appears to not have weights")
             unpack = np.vectorize(lambda t: unpack_tracer_data_no_weights(t))
-        return unpack(h5f['tracers'][...])
+            return unpack(h5f['tracers'][...])
 
     def stack(self):
         return np.column_stack(self.unpack())
@@ -30,15 +32,15 @@ class TracerData_t:
         return self.unpack()[0]
 
     def radii(self):
-        (ID, x, y, vx, vy, rho, p) = self.unpack()
+        (ID, x, y, vx, vy, rho, p, w) = self.unpack()
         return np.sqrt(x * x + y * y)
 
     def velocities(self):
-        (ID, x, y, vx, vy, rho, p) = self.unpack()
+        (ID, x, y, vx, vy, rho, p, w) = self.unpack()
         return np.sqrt(vx * vx + vy * vy)
 
     def radial_velocities(self):
-        (ID, x, y, vx, vy, rho, p) = self.unpack()
+        (ID, x, y, vx, vy, rho, p, w) = self.unpack()
         r = self.radii()
         return (x * vx + y * vy) / r
 
@@ -52,19 +54,19 @@ class TracerData_t:
             print("Provided tracer file doesn't have weights")
 
     def angular_momentum_density(self):
-        (ID, x, y, vx, vy, rho, p) = self.unpack()
+        (ID, x, y, vx, vy, rho, p, w) = self.unpack()
         r = np.column_stack((x , y))
         v = np.column_stack((vx, vy))
         return np.cross(r, v) * rho
 
     def specific_angular_momentum(self):
-        (ID, x, y, vx, vy, rho, p) = self.unpack()
+        (ID, x, y, vx, vy, rho, p, w) = self.unpack()
         r = np.column_stack((x , y))
         v = np.column_stack((vx, vy))
         return np.cross(r, v)
 
     def distance_to_component_1(self):
-        (ID, x, y, vx, vy, rho, p) = self.unpack()
+        (ID, x, y, vx, vy, rho, p, w) = self.unpack()
         Omega = 1.0
         time  = self.time()
         xh = 0.5 * np.cos(Omega * time)
@@ -72,7 +74,7 @@ class TracerData_t:
         return np.sqrt((x - xh)**2 + (y - yh)**2)
 
     def distance_to_component_2(self):
-        (ID, x, y, vx, vy, rho, p) = self.unpack()
+        (ID, x, y, vx, vy, rho, p, w) = self.unpack()
         Omega = 1.0
         time  = self.time()
         xh = - 0.5 * np.cos(Omega * time)
@@ -102,4 +104,5 @@ def unpack_tracer_data_no_weights(tracer):
             tracer[vel][0], # vx       (3)
             tracer[vel][1], # vy       (4)
             tracer[2],      # density  (5)
-            tracer[3])      # pressure (6)
+            tracer[3],      # pressure (6)
+            -1.0)            # weight
